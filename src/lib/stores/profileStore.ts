81ec5623c7888993380870cc7e711e1ebbf17efe
Ximena-21 <ximenanzola.21@gmail.com>
2023-04-13 20:28:59 -0500

    fixing deploy

--- src/lib/stores/profileStore.ts
+++ src/lib/stores/profileStore.ts
@@ -14,6 +14,7 @@ interface  IProfileStore {
     urls: IUrl[]
     getDataUrls: () => void
     addUrl: (data: IUrlForm) => void
+    deleteUrl: (id: number) => void
 }
 
 const getDataUrls = (set : any, get: any)  => async () => {    
