4891f7734e89b5152fd956cf5cbcd34b1c85d877
Ximena-21 <ximenanzola.21@gmail.com>
2023-04-14 18:11:35 -0500

    changes style login and url

--- src/views/profile/index.tsx
+++ src/views/profile/index.tsx
@@ -10,9 +10,10 @@ import {
   buttonLoginStyles,
   gridStyles,
   buttonStyles,
+  containerWallStyles,
 } from "./styles";
 import { useProfileStore } from "../../lib/stores/profileStore";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useUserStore } from "../../lib/stores/userStore";
 
 export interface IUrlForm {
@@ -62,14 +63,7 @@ export const Profile = () => {
       <ProfileUser />
 
       <div
-        style={{
-          width: "100%",
-          maxWidth: "750px",
-          display: "flex",
-          gap: "50px",
-          flexWrap: "wrap",
-          justifyContent: "center",
-        }}
+        style={containerWallStyles}
       >
         <form onSubmit={handleSubmit(onSubmit)}>
           <Controller
